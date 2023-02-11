100ef882ac64f3cad0b03fb5a5b36805b5ef4bf6
nigelarue <larue.nigel91@gmail.com>
2023-02-11 15:27:16 -0600

    updated password so that users can be prompted to include special characters.

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
-// Variables for setting password
-// Button variable
 var genBtn = document.querySelector("#generate");
-// Available characters to use for generating password 
-var psswrdCharacters =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()+-_";
-// Password length
-var passwordLength = 8;
-var password = "";
 
-// Add event listener to generate button
 genBtn.addEventListener("click", genPassword);
 
-
-// Write function to generate password upond click event. This does not satisfy the user story because I have not figured out how to include user inputted prompts to describe length and character inputs.
-
 function genPassword() {
-  var psswrdCharacters =
-    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var password = "";
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * psswrdCharacters.length);
-    password += psswrdCharacters.substring(randomNumber, randomNumber + 1);
-  }
-  // query selector to pull from HTML
-  document.getElementById("password").value = password;
+  var passwordLength = parseInt(prompt("Enter the password length (8-128):"));
 
-  for (var i = 0; i <= passwordLength; i++) {
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return;
+  }
+
+  var includeLowercase = prompt("Include lowercase characters? (yes/no)");
+  if (includeLowercase === "yes") {
+    includeLowercase = true;
+  } else {
+    includeLowercase = false;
+  }
+
+  var includeUppercase = prompt("Include uppercase characters? (yes/no)");
+  if (includeUppercase === "yes") {
+    includeUppercase = true;
+  } else {
+    includeUppercase = false;
+  }
+
+  var includeNumeric = prompt("Include numeric characters? (yes/no)");
+  if (includeNumeric === "yes") {
+    includeNumeric = true;
+  } else {
+    includeNumeric = false;
+  }
+
+  var includeSpecial = prompt("Include special characters? (yes/no)");
+  if (includeSpecial === "yes") {
+    includeSpecial = true;
+  } else {
+    includeSpecial = false;
+  }
+
+  var psswrdCharacters = "";
+  if (includeLowercase) {
+    psswrdCharacters += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (includeUppercase) {
+    psswrdCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (includeNumeric) {
+    psswrdCharacters += "0123456789";
+  }
+  if (includeSpecial) {
+    psswrdCharacters += "!@#$%^&*()+-_";
+  }
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * psswrdCharacters.length);
     password += psswrdCharacters.substring(randomNumber, randomNumber + 1);
   }
-  return password;
+
+  document.getElementById("password").value = password;
 }
